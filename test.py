d2183edb5edbae86ab54b4bcaf2e3d06b3aedbbb
Francesco Gianferrari Pini <francesco.gianferraripini@quantyca.it>
2018-11-03 00:25:13 +0100

    Ok

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-,
+from fastai import Learner
+
+
+class QModelPackager:
+    def __init__(self, url:str, learner:Learner = None): 
+        self.url = url
+
+x = QModelPackager('Ciao')
+print(x.url)
